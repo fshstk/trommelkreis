a8b1389133bcc4ff18acfc93cff7ae39fa8a73c7
fshstk <fshstk@fshstk.com>
2020-03-30 11:25:13 +0200

    change all .html suffixes to .jinja (so vs code highlights the code correctly)

--- __init__.py
+++ __init__.py
@@ -5,27 +5,28 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.jinja')
 
 @app.route('/info')
 def info():
-    return render_template('info.html')
+    return render_template('info.jinja')
 
 @app.route('/challenge')
 def challenge():
-    return render_template('challenge.html')
+    return render_template('challenge.jinja')
 
 @app.route('/archiv')
-def archiv():
-    return render_template('archiv.html')
+@app.route('/archive')
+def archive():
+    return render_template('archive.jinja')
 
 @app.route('/session')
 def session():
-    return render_template('session.html')
+    return render_template('session.jinja')
 
 @app.route('/upload')
 def upload():
-    return render_template('upload.html')
+    return render_template('upload.jinja')
 
 if __name__ == '__main__':
     app.run(debug = True)
