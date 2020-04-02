965ca3591523664e44a1cf77490e6ec05747e806
fshstk <fshstk@fshstk.com>
2020-04-02 21:55:45 +0200

    add config file

--- trommelkreis/__init__.py
+++ trommelkreis/__init__.py
@@ -5,6 +5,7 @@ from .archive import SessionCollection
 
 locale.setlocale(locale.LC_ALL, "de_AT.UTF-8")
 app = Flask(__name__)
-            
+app.config.from_pyfile('config.py')
+
 from . import views
 from .vars import *
