5c016c597467cd2eecbd7dfda276e55a7ce5d8cc
fshstk <fshstk@fshstk.com>
2020-04-19 12:58:28 +0200

    add passenger_wsgi script

---
+++ passenger_wsgi.py
@@ -0,0 +1,9 @@
+import sys, os
+INTERP = "/home/trommelkreis_v2/opt/python-3.7.7/bin/python3"
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+sys.path.append(os.getcwd())
+
+import trommelkreis
+
+application = trommelkreis.app
