060b97b238fa076d384106cadb84b588bb4a4dba
fshstk <fshstk@fshstk.com>
2020-04-29 13:56:11 +0200

    tweak trommelkreis/* files to work with new sql-based archive (no actual implementation yet)

--- trommelkreis/views.py
+++ trommelkreis/views.py
@@ -1,13 +1,11 @@
-from flask import render_template, abort, request, send_from_directory, send_file
+from flask import render_template, abort, request, send_file
 import os
 from datetime import datetime
 from zipfile import ZipFile
 import io
-
-# from werkzeug import FileWrapper
-
+from sqlalchemy.orm.exc import NoResultFound
 from . import app
-from .vars import sessions
+
 
 ################################################################################
 
@@ -26,9 +24,7 @@ def info():
 @app.route("/archiv")
 @app.route("/archiv/")
 def archive():
-    return render_template(
-        "archive.jinja", archive=sessions.grouped_by_month
-    )  # TODO: old method
+    return render_template("archive.jinja", archive=Session.grouped_by_month)
 
 
 @app.route("/abo")
@@ -36,83 +32,80 @@ def subscribe():
     return render_template("subscribe.jinja")
 
 
-@app.route("/challenge")
-def challenge():
-    return render_template("challenge.jinja")
-
-
-################################################################################
-
-# single session:
-@app.route("/archiv/<int:number>")
-def session(number):
-    session = sessions.get_session_by_yyyymmdd(number)  # TODO: old method
-    if session is not None:
-        return render_template("session.jinja", session=session)
-    else:
-        return abort(400)
-
-
-# entire session as zip file:
-@app.route("/archiv/<int:number>.zip")
-# @app.route('/archiv/<int:number>/download/<whatever>') # (debug) chrome likes to cache downloads
-def download_session(number, whatever):
-    session = sessions.get_session_by_yyyymmdd(number)  # TODO: old method
-    if session is not None:
-        data = io.BytesIO()
-        with ZipFile(data, "w") as zipfile:
-            for file in session.files:
-                zipfile.write(
-                    file.path, os.path.basename(file.path)
-                )  # TODO: old method
-        data.seek(0)
-        return send_file(
-            data,
-            mimetype="application/zip",
-            as_attachment=True,
-            attachment_filename=str(number) + ".zip",
-        )
-    else:
-        return abort(400)
-
-
-# single mp3 from session:
-@app.route("/archiv/<int:number>/<filename>")
-def download_file(number, filename):
-    session = sessions.get_session_by_yyyymmdd(number)  # TODO: old method
-    if session is not None:
-        file = session.get_file_by_name(filename)  # TODO: old method
-        if file is not None:
-            return send_file(file.path)
-        else:
-            return abort(400)
-    else:
-        return abort(400)
-
-
-################################################################################
-
-from .archive import Session
-
-today = Session("20200401")  # TODO: old method
-# today = None
-
-
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
-    # try:
-    #     today = sessions.get_session_by_yyyymmdd(datetime.today())  # TODO: old method
-    # except:
-    #     today = None
-
-    if today is not None:
-        if request.method == "POST":
-            # save uploaded file here...
-            return render_template("home.jinja")
-        else:
-            return render_template("upload.jinja", today=today)
-    else:
+    try:
+        today = Session.query.filter_by(date=datetime.today())
+    except NoResultFound:
         return render_template("nexttime.jinja")
+
+    if request.method == "POST":
+        # save uploaded file here...
+        return render_template("home.jinja")
+    else:
+        return render_template("upload.jinja", today=today)
+
+
+################################################################################
+
+
+# Show single session:
+@app.route("/archiv/<name>")
+def session(name):
+    try:
+        session = Session.query.filter_by(name=name).one()
+    except NoResultFound:
+        return abort(400)
+    return render_template("session.jinja", session=session)
+
+
+# Get all session files as zip archive:
+@app.route("/archiv/<name>.zip")
+# @app.route('/archiv/<int:number>/download/<whatever>') # (debug) chrome likes to cache downloads
+def download_session(name, whatever):
+    try:
+        session = Session.from_name(name)
+    except NoResultFound:
+        return abort(400)
+    return send_files_as_zip(session.files, name=name)
+
+
+# Get single MP3 from session:
+@app.route("/archiv/<name>/<filename>")
+def download_file(name, filename):
+    try:
+        session = Session.from_name(name)
+    except NoResultFound:
+        return abort(400)
+
+    try:
+        file = session.get_file_by_name(filename)
+    except NoResultFound:
+        return abort(400)
+
+    return send_file(
+        file.data,
+        mimetype="audio/mpeg",  # TODO: remove if using types other than mp3 files
+        as_attachment=True,
+        attachment_filename=file.filename,
+    )
+
+
+################################################################################
+
+
+def send_files_as_zip(filelist, name):
+    zipdata = io.BytesIO()
+    with ZipFile(zipdata, "w") as zipfile:
+        for file in filelist:
+            zipfile.write(file.data, file.filename)
+    zipdata.seek(0)
+    return send_file(
+        zipdata,
+        mimetype="application/zip",
+        as_attachment=True,
+        attachment_filename="{}.zip".format(name),
+    )
 
 
 ################################################################################
@@ -122,16 +115,3 @@ def upload():
 @app.errorhandler(404)
 def notfound(error):
     return render_template("error.jinja")
-
-
-################################################################################
-
-
-@app.route("/testfile")
-def testfile():
-    return send_from_directory(app.config["ARCHIVE_PATH"], "test.mp3")
-
-
-@app.route("/test")
-def testpage():
-    return render_template("test.jinja")
