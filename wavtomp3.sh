7efff0ddce7214800f7e6f3b89c85cddbb2ec7cc
fshstk <fshstk@fshstk.com>
2020-05-01 04:16:22 +0200

    make wavtomp3 script actually work

--- wavtomp3.sh
+++ wavtomp3.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-file="$1"
-
-# find . -name '*.wav' -maxdepth 2 -exec /usr/local/bin/lame -V 0 -q 0 '{}' \;
-# find . -name '*.wav' -maxdepth 2 -exec rm '{}' \;
-find . -name '*.mp3' -maxdepth 2 -exec echo 'files/{}' \;
+# find . -name '*.wav' -maxdepth 3 -exec echo '{}' \;
+find . -name '*.wav' -maxdepth 3 -exec /usr/local/bin/lame -V 0 -q 0 '{}' \;
+find . -name '*.wav' -maxdepth 3 -exec rm '{}' \;
