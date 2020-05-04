6e1a48afdae1bede07a6e929452eac20fc14a06d
fshstk <fshstk@fshstk.com>
2020-05-04 15:42:44 +0200

    remove old (flask) index.py entry point and modify passenger_wsgi.py to launch django project

--- index.py
+++
@@ -1,3 +0,0 @@
-from trommelkreis import app
-
-app.run(host='0.0.0.0', debug = True)
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,21 @@
+# Django version of passenger_wsgi.py
 import sys, os
 
 INTERP = "/home/trommelkreis_v2/opt/python-3.7.7/bin/python3"
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
-sys.path.append(os.getcwd())
 
-import trommelkreis
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + "/trommelkreis")
 
-application = trommelkreis.app
+# Do virtualenv stuff here:
+# sys.path.insert(0, cwd + "/home/trommelkreis_v2/opt/python-3.7.7/bin")
+# sys.path.insert(
+#     0, cwd + "/home/trommelkreis_v2/opt/python-3.7.7/lib/python3.7/site-packages/"
+# )
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "trommelkreis.settings"
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
