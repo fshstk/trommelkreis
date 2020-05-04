521127cc82750e8c14e60bc025fdcece7cccfdd5
fshstk <fshstk@fshstk.com>
2020-05-04 16:02:21 +0200

    add correct database credentials (using test database for now)

--- trommelkreis/trommelkreis/settings.py
+++ trommelkreis/trommelkreis/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "trommelkreis.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "trommelkreis_test",
+        "USER": "trommelkreis",
+        "PASSWORD": "YUM-senk8nect",
+        "HOST": "data.trommelkreis.club",
+        "PORT": "3306",
     }
 }
 
