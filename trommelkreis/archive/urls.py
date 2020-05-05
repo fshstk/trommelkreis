7d2d67ba488de2ca58f4ed47075022c3705996b5
fshstk <fshstk@fshstk.com>
2020-05-05 12:13:13 +0200

    add archive routing and method stubs

--- trommelkreis/archive/urls.py
+++ trommelkreis/archive/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<session>.zip", views.download_session, name="zipdownload"),
+    path("<session>/", views.show_session, name="sessionview"),
+    path("<session>/<file>", views.download_file, name="fileview"),
 ]
--- trommelkreis/archive/views.py
+++ trommelkreis/archive/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
+from .models import Challenge, Session, Artist, AudioFile
 
 
 def index(request):
-    return HttpResponse("hello world")
+    """Show archive index."""
+    pass
+
+
+def download_session(request, session):
+    """Get all session files as zip archive."""
+    try:
+        pass  # look for session
+    except Session.DoesNotExist:
+        raise Http404("Session does not exist.")
+
+
+def show_session(request, session):
+    """Show single session."""
+    try:
+        pass  # look for session
+    except Session.DoesNotExist:
+        raise Http404("Session does not exist.")
+
+
+def download_file(request, session, file):
+    """Get single MP3 from session."""
+    try:
+        pass  # look for session
+    except Session.DoesNotExist:
+        raise Http404("Session does not exist.")
+
+    try:
+        pass  # look for file
+    except AudioFile.DoesNotExist:
+        raise Http404("File does not exist.")
