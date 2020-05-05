c8cf0162f0a96048b53e42e670ce7dba5787d91d
fshstk <fshstk@fshstk.com>
2020-05-05 03:36:42 +0200

    add newly created database classes to admin panel

--- trommelkreis/archive/admin.py
+++ trommelkreis/archive/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Challenge, Session, Artist, AudioFile
+
+admin.site.register(Challenge)
+admin.site.register(Session)
+admin.site.register(Artist)
+admin.site.register(AudioFile)
