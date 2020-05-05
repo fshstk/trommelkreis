0a8241700d2c49ce0c9a93dd2b09607aae8cef88
fshstk <fshstk@fshstk.com>
2020-05-05 03:25:34 +0200

    first draft of new archive data model

--- trommelkreis/archive/models.py
+++ trommelkreis/archive/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Challenge(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    blurb = models.CharField(max_length=1000, blank=True)
+    longtext = models.CharField(max_length=5000, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Session(models.Model):
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.date
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AudioFile(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, blank=True, null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=200)
+    duration = models.IntegerField()
+    data = models.FileField(upload_to="archive/%Y%m%d/")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["session", "name"], name="unique_name_per_session"
+            )
+        ]
+
+    def __str__(self):
+        return self.name
