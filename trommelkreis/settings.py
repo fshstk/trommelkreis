8f2b2e82e5cf5564c53ddb7945fc04d2d473019b
fshstk <fshstk@fshstk.com>
2020-05-05 12:13:55 +0200

    move all django project files (back) to base dir and update passenger_wsgi.py

--- trommelkreis/settings.py
+++ trommelkreis/settings.py
@@ -133,4 +133,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.dirname(BASE_DIR) + "/public/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "public", "static")
