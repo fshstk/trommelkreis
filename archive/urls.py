e5fe33d6c165d7054e6edca8e87098b67c35fb6c
fshstk <fshstk@fshstk.com>
2020-05-07 03:28:00 +0200

    change "session_index" to "archive_home" in url routings since it makes more sense

--- archive/urls.py
+++ archive/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.show_all_sessions, name="session_index"),
+    path("", views.show_all_sessions, name="archive_home"),
     path("<session>.zip", views.download_session, name="session_zip"),
     path("<session>/", views.show_single_session, name="session_view"),
 ]
