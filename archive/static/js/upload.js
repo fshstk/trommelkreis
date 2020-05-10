725abbc9e225207c2eba1cf541a4608951385b2e
fshstk <fshstk@fshstk.com>
2020-05-11 01:29:58 +0200

    client-side ID3 tag parsing works!

--- archive/static/js/upload.js
+++ archive/static/js/upload.js
@@ -1,22 +1,41 @@
 "use strict";
 
-(function () {
-  window.addEventListener(
-    "load",
+// Validate form when clicking submit:
+$("form").submit(function (event) {
+  if ($("form")[0].checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  if (true) {
+    $("#id_password");
+  }
+  $("form").addClass("was-validated");
+});
+
+$(".custom-file-input").on("change", function () {
+  let file = $(this).prop("files")[0];
+
+  // Populate title/artist fields with ID3 tags of selected file:
+  ID3.loadTags(
+    file.name,
     function () {
-      let form = document.getElementById("upload-form");
-      form.addEventListener(
-        "submit",
-        function (event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add("was-validated");
-        },
-        false
-      );
+      let tags = ID3.getAllTags(file.name);
+      console.log("Artist: " + tags.artist);
+      console.log("Title: " + tags.title);
+      if (tags.title) {
+        var title = tags.title;
+      } else {
+        // Strip file suffix:
+        var title = file.name.replace(/\.[^/.]+$/, "");
+        // Underscores to spaces:
+        title = file.name.replace(/_/, " ");
+      }
+      $("#id_name").val(title);
+      $("#id_artist").val(tags.artist);
     },
-    false
+    { dataReader: ID3.FileAPIReader(file) }
   );
-})();
+
+  // Display the file name in input field if one is selected:
+  $(this).next(".custom-file-label").html(file.name);
+});
