d6be4e4912990c9ea97969537f178e5fbb77dd3a
fshstk <fshstk@fshstk.com>
2020-05-11 03:02:49 +0200

    "title" form element is now hidden, but populated client-side by the following algorithm:
    if title id3 tag is present, use that
    else take filename, strip away suffix, convert underscores to space

--- archive/static/js/upload.js
+++ archive/static/js/upload.js
@@ -40,27 +40,25 @@ $("form").submit(function (event) {
 $(".custom-file-input").on("change", function () {
   let file = $(this).prop("files")[0];
 
-  // Populate title/artist fields with ID3 tags of selected file:
   ID3.loadTags(
     file.name,
     function () {
       let tags = ID3.getAllTags(file.name);
-      console.log("Artist: " + tags.artist);
-      console.log("Title: " + tags.title);
       if (tags.title) {
         var title = tags.title;
       } else {
         // Strip file suffix:
         var title = file.name.replace(/\.[^/.]+$/, "");
         // Underscores to spaces:
-        title = file.name.replace(/_/, " ");
+        title = title.replace(/_/, " ");
       }
+      // Populate title/artist fields with ID3 tags of selected file:
       $("#id_name").val(title);
+      console.log(title);
       $("#id_artist").val(tags.artist);
+      // Display the file name in input field if one is selected:
+      $(".custom-file-label").html(title);
     },
     { dataReader: ID3.FileAPIReader(file) }
   );
-
-  // Display the file name in input field if one is selected:
-  $(this).next(".custom-file-label").html(file.name);
 });
