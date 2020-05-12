8c0141c20b901a822d778152fb0757f66325319c
fshstk <fshstk@fshstk.com>
2020-05-12 04:32:30 +0200

    remove last remaining flask files

--- trommelkreis_flask/config.py
+++
@@ -1,9 +0,0 @@
-# Disabled to suppress warning at startup:
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-SQLALCHEMY_DATABASE_URI = "mysql://{user}:{password}@{host}/{db}".format(
-    user="trommelkreis",
-    password="YUM-senk8nect",
-    host="data.trommelkreis.club",
-    db="trommelkreis",
-)
--- trommelkreis_flask/vars.py
+++
@@ -1,6 +0,0 @@
-from . import app
-
-
-@app.context_processor
-def global_vars():
-    return {}
