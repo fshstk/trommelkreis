3bf1829d5af8a450b99d255c8c6efebbdff8148c
fshstk <fshstk@fshstk.com>
2020-05-12 03:42:02 +0200

    move global static files from /archive/static to /trommelkreis/static

--- trommelkreis/settings.py
+++ trommelkreis/settings.py
@@ -134,5 +134,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "public", "static")
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "trommelkreis", "static")]
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "public", "media")
