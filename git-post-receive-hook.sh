dd22855ec0a2c118d75decb6c2becc73112258d8
fshstk <fshstk@fshstk.com>
2020-05-12 03:24:45 +0200

    slightly fancier post-receive-hook

--- git-post-receive-hook.sh
+++ git-post-receive-hook.sh
@@ -1,15 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-# Checkout current branch to ~/www/:
-git --work-tree=/home/trommelkreis_v2/www --git-dir=/home/trommelkreis_v2/web.git checkout -f
+BRANCH="dh-jango"
+PYTHON="/home/trommelkreis_v2/opt/python-3.7.7/bin/python3"
+MANAGE="/home/trommelkreis_v2/www/manage.py"
 
+printf "Check out branch %s to ~/www/...\n" $BRANCH
+git --work-tree=/home/trommelkreis_v2/www --git-dir=/home/trommelkreis_v2/web.git checkout -f $BRANCH
 
-# If django script exists, collect static files and migate database:
-if test -f "/home/trommelkreis_v2/www/manage.py"; then
-    /home/trommelkreis_v2/opt/python-3.7.7/bin/python3 /home/trommelkreis_v2/www/manage.py collectstatic --no-input
-    /home/trommelkreis_v2/opt/python-3.7.7/bin/python3 /home/trommelkreis_v2/www/manage.py migrate --no-input
+printf "Collect static files and migrate database...\n"
+if test -f $MANAGE; then
+    $PYTHON $MANAGE collectstatic --no-input
+    $PYTHON $MANAGE migrate --no-input
 fi
 
-# Restart server:
+printf "Restarting server...\n"
 touch /home/trommelkreis_v2/www/tmp/restart.txt
-echo "Server restarted."
+
+printf "Done\n"
