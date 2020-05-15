cbbaef4e82955f815b0c9fe2f0864c6b4b642bf7
fshstk <fshstk@fshstk.com>
2020-05-15 22:01:39 +0200

    minor cosmetic changes in post-receive-hook shell script

--- git-post-receive-hook.sh
+++ git-post-receive-hook.sh
@@ -4,10 +4,10 @@ BRANCH="dh-jango"
 PYTHON="/home/trommelkreis_v2/opt/python-3.7.7/bin/python3"
 MANAGE="/home/trommelkreis_v2/www/manage.py"
 
-printf "Check out branch %s to ~/www/...\n" $BRANCH
+printf "Checking out branch %s to ~/www/...\n" $BRANCH
 git --work-tree=/home/trommelkreis_v2/www --git-dir=/home/trommelkreis_v2/web.git checkout -f $BRANCH
 
-printf "Collect static files and migrate database...\n"
+printf "Collecting static files and migrate database...\n"
 if test -f $MANAGE; then
     $PYTHON $MANAGE collectstatic --no-input
     $PYTHON $MANAGE migrate --no-input
