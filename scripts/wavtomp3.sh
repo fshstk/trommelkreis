36503e48060370372919231ea3ed9f591b3508cb
fshstk <fshstk@fshstk.com>
2020-05-18 23:24:26 +0200

    remove wav2mp3.sh script (no longer needed)

--- scripts/wavtomp3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# find . -name '*.wav' -maxdepth 3 -exec echo '{}' \;
-find . -name '*.wav' -maxdepth 3 -exec /usr/local/bin/lame -V 0 -q 0 '{}' \;
-find . -name '*.wav' -maxdepth 3 -exec rm '{}' \;
