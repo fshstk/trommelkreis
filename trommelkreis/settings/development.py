4866f726cab6c3be3ef8fcd8622dcd68c586f244
fshstk <fshstk@fshstk.com>
2020-05-24 17:32:25 +0200

    switch trommelkreis_test database with proper production and (local) test databases

--- trommelkreis/settings/development.py
+++ trommelkreis/settings/development.py
@@ -14,18 +14,20 @@ ALLOWED_HOSTS = [
 # Database
 
 DATABASES = {
+    # Local development DB:
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "trommelkreis_test",
-        "USER": "trommelkreis",
-        "PASSWORD": get_secret("DB_PASSWORD"),
-        "HOST": "data.trommelkreis.club",
+        "NAME": "trommelkreis_dev",
+        "USER": "root",
+        "PASSWORD": get_secret("DEV_DB_PASSWORD"),
+        "HOST": "localhost",
         "PORT": "3306",
         "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     },
-    # "archive": {
+    # TEST DATABASE:
+    # "test_archive": {
     #     "ENGINE": "django.db.backends.mysql",
-    #     "NAME": "trommelkreis_archive",
+    #     "NAME": "trommelkreis_test",
     #     "USER": "trommelkreis",
     #     "PASSWORD": get_secret("DB_PASSWORD"),
     #     "HOST": "data.trommelkreis.club",
--- trommelkreis/settings/production.py
+++ trommelkreis/settings/production.py
@@ -14,22 +14,14 @@ ALLOWED_HOSTS = [
 # Database
 
 DATABASES = {
+    # PRODUCTION DATABASE:
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "trommelkreis_test",
+        "NAME": "trommelkreis",
         "USER": "trommelkreis",
         "PASSWORD": get_secret("DB_PASSWORD"),
         "HOST": "data.trommelkreis.club",
         "PORT": "3306",
         "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     },
-    # "archive": {
-    #     "ENGINE": "django.db.backends.mysql",
-    #     "NAME": "trommelkreis_archive",
-    #     "USER": "trommelkreis",
-    #     "PASSWORD": get_secret("DB_PASSWORD"),
-    #     "HOST": "data.trommelkreis.club",
-    #     "PORT": "3306",
-    #     "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-    # },
 }
