fc062eca9b516b1ed29f2ce0933fa98f904cb6e6
fshstk <fshstk@fshstk.com>
2020-07-08 02:45:56 +0200

    additional improvements to admin list view

--- archive/admin.py
+++ archive/admin.py
@@ -6,16 +6,23 @@ from archive.models import Challenge, Session, Artist, AudioFile
 
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
+    date_hierarchy = "date"
     list_display = ("challenge", "number_of_tracks", "date")
+    # list_filter = ("challenge",)
+    search_fields = ["challenge__name"]
     prepopulated_fields = {"slug": ("date",)}
 
     def number_of_tracks(self, obj):
         return obj.audiofile_set.count()
 
+    # number_of_tracks.admin_order_field = Count("audiofile")
+
 
 @admin.register(Challenge)
 class ChallengeAdmin(admin.ModelAdmin):
     list_display = ("name", "number_of_sessions", "copyright_ok")
+    # list_filter = ("copyright_ok",)
+    search_fields = ["name"]
     prepopulated_fields = {"slug": ("name",)}
 
     def number_of_sessions(self, obj):
@@ -30,6 +37,7 @@ class ChallengeAdmin(admin.ModelAdmin):
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ("name", "number_of_tracks")
+    search_fields = ["name"]
     prepopulated_fields = {"slug": ("name",)}
 
     def number_of_tracks(self, obj):
@@ -38,7 +46,11 @@ class ArtistAdmin(admin.ModelAdmin):
 
 @admin.register(AudioFile)
 class AudioFileAdmin(admin.ModelAdmin):
-    list_display = ("filename", "artist", "duration", "challenge", "date")
+    date_hierarchy = "session__date"
+    list_display = ("date", "challenge", "filename", "artist", "duration")
+    list_display_links = ("filename",)
+    # list_filter = ("artist",)
+    search_fields = ["name", "session__challenge__name", "artist__name"]
     prepopulated_fields = {"slug": ("name",)}
 
     def duration(self, obj):
@@ -47,5 +59,9 @@ class AudioFileAdmin(admin.ModelAdmin):
     def challenge(self, obj):
         return obj.session.challenge
 
+    challenge.admin_order_field = "session__challenge__name"
+
     def date(self, obj):
         return obj.session.date
+
+    date.admin_order_field = "session__date"
