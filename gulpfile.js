0bad15fe750dbfc81720fd076609d0c65d089cd2
fshstk <fshstk@fshstk.com>
2021-04-11 01:22:04 +0200

    Add gulp workflow for including JS libraries

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const { series, src, dest } = require("gulp");
+
+function fonts() {
+    const files = [
+        "node_modules/@fortawesome/fontawesome-free/webfonts/*",
+        "node_modules/ubuntu-fontface/fonts/*",
+    ];
+    return src(files).pipe(dest("_fonts"));
+}
+
+function scripts() {
+    const files = [
+        "node_modules/jquery/dist/jquery.min.js",
+        "node_modules/popper.js/dist/popper.min.js",
+        "node_modules/bootstrap/dist/js/bootstrap.min.js",
+    ];
+    return src(files).pipe(dest("_scripts"));
+}
+
+// By default, do nothing:
+// (This is called when python manage.py runserver is executed.)
+exports.default = (cb) => { cb(); };
+
+// (This is called when python manage.py collectstatic is executed.)
+exports.build = series(fonts, scripts);
