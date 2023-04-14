4bec5d60b092466576ed352c91a2d51023aa515a
fshstk <fshstk@fshstk.com>
2023-04-14 18:23:20 +0200

    Add FontAwesome support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+  import '@fortawesome/fontawesome-free/js/all.js'
+</script>
+
+<h1>Welcome to SvelteKit <i class="fa-solid fa-user"></i></h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
