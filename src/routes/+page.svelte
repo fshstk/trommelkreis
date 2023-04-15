bbd7465bd0e972ff9c45577a40188ef7a74c4b39
fshstk <fshstk@fshstk.com>
2023-04-15 04:34:44 +0200

    Working home page prototype

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,38 @@
 <script>
-  import '@fortawesome/fontawesome-free/js/all.js'
+	import '@fortawesome/fontawesome-free/js/all';
 </script>
 
-<h1>Welcome to SvelteKit <i class="fa-solid fa-user"></i></h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav class="nav container p-1">
+	<a class="nav-link text-dark" href="#" title="Home">
+		<i class="fa-solid fa-house" />
+	</a>
+	<a class="nav-link text-dark" href="#">Info</a>
+	<a class="nav-link text-dark" href="#">Archiv</a>
+	<a class="nav-link text-primary" href="#">Mitmachen</a>
+	<a class="nav-link text-dark ms-auto" href="#" title="Newsletter">
+		<i class="fa-solid fa-envelope" />
+	</a>
+</nav>
+
+<main class="container bg-white p-3">
+	<header>
+		<div class="text-center">
+			<h1 class="font-weight-bold">Digitaler Trommelkreis</h1>
+			<h6 class="text-muted">Jeden Montag, 20:00-22:00 Uhr</h6>
+			<img src="logo.png" width="700px" class="img-fluid p-3" alt="logo" />
+		</div>
+	</header>
+
+	<!-- content here -->
+</main>
+
+<footer class="container text-center p-1 small text-dark">
+	Impressum: &copy; 2023 Fabian Hummel |
+	<a href="https://github.com/fshstk/trommelkreis">github.com/fshstk/trommelkreis</a>
+</footer>
+
+<style>
+	:global(body) {
+		background-color: #ddd;
+	}
+</style>
