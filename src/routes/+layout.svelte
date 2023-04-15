7fd13de92e1873d05b4edc4bd514afc76eef9020
fshstk <fshstk@fshstk.com>
2023-04-15 12:38:30 +0200

    Move common elements to layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,23 @@
 	import '../app.scss';
 </script>
 
+<nav class="nav container p-1">
+	<a class="nav-link text-dark" href="#" title="Home">
+		<i class="fa-solid fa-house" />
+	</a>
+	<a class="nav-link text-dark" href="#">Info</a>
+	<a class="nav-link text-dark" href="#">Archiv</a>
+	<a class="nav-link text-primary" href="#">Mitmachen</a>
+	<a class="nav-link text-dark ms-auto" href="#" title="Newsletter">
+		<i class="fa-solid fa-envelope" />
+	</a>
+</nav>
+
 <slot />
+
+<footer class="container text-center p-1 small text-dark">
+	Impressum: &copy; 2023 Fabian Hummel |
+	<a class="text-decoration-none" href="https://github.com/fshstk/trommelkreis">
+		github.com/fshstk/trommelkreis
+	</a>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,3 @@
-<script>
-	import '@fortawesome/fontawesome-free/js/all';
-	import '@fontsource/ubuntu/700.css';
-	import '@fontsource/ubuntu/400.css';
-</script>
-
-<nav class="nav container p-1">
-	<a class="nav-link text-dark" href="#" title="Home">
-		<i class="fa-solid fa-house" />
-	</a>
-	<a class="nav-link text-dark" href="#">Info</a>
-	<a class="nav-link text-dark" href="#">Archiv</a>
-	<a class="nav-link text-primary" href="#">Mitmachen</a>
-	<a class="nav-link text-dark ms-auto" href="#" title="Newsletter">
-		<i class="fa-solid fa-envelope" />
-	</a>
-</nav>
-
 <main class="container bg-white p-3">
 	<header>
 		<div class="text-center">
@@ -27,22 +9,3 @@
 
 	<!-- content here -->
 </main>
-
-<footer class="container text-center p-1 small text-dark">
-	Impressum: &copy; 2023 Fabian Hummel |
-	<a class="text-decoration-none" href="https://github.com/fshstk/trommelkreis">
-		github.com/fshstk/trommelkreis
-	</a>
-</footer>
-
-<style>
-	:global(body) {
-		background-color: #ddd;
-		font-family: 'Ubuntu', sans-serif;
-	}
-
-	h1,
-	h6 {
-		font-weight: 700;
-	}
-</style>
