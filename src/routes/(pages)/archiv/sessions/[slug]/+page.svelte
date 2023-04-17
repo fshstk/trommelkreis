218c7ff79637f4d7456c4c21a0175e97efab5191
fshstk <fshstk@fshstk.com>
2023-04-17 15:17:13 +0200

    Add single session page

---
+++ src/routes/(pages)/archiv/sessions/[slug]/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import FileItem from './FileItem.svelte';
+
+	export let data;
+</script>
+
+<div class="card">
+	<div class="card-body">
+		{data.session.challenge.description}
+	</div>
+</div>
+
+<hr />
+
+<div class="list-group text-left">
+	{#each data.session.files as file}
+		<FileItem {file} />
+	{/each}
+</div>
+
+<hr />
+
+<div class="d-flex justify-content-between">
+	<span>
+		<a href="/archiv">Archiv</a> / <a href="/archiv/sessions">Sessions</a> / {data.session.slug}
+	</span>
+</div>
---
+++ src/routes/(pages)/archiv/sessions/[slug]/FileItem.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let file;
+</script>
+
+<a href="#" class="list-group-item disabled bg-secondary">
+	<div class="d-flex justify-content-between">
+		<h5 class="mb-1 text-truncate text-white">{file.name}</h5>
+	</div>
+	<div class="d-flex justify-content-between">
+		<p class="mb-1 text-truncate text-white">{file.artist}</p>
+		<small class="text-warning">verloren</small>
+	</div>
+</a>
