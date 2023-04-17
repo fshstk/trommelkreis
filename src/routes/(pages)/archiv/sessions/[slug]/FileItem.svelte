837d91032642487a9fc8e10b071a1ebb45e33c95
fshstk <fshstk@fshstk.com>
2023-04-17 17:10:27 +0200

    Provide default value if no artist is defined

--- src/routes/(pages)/archiv/sessions/[slug]/FileItem.svelte
+++ src/routes/(pages)/archiv/sessions/[slug]/FileItem.svelte
@@ -7,7 +7,7 @@
 		<h5 class="mb-1 text-truncate text-white">{file.name}</h5>
 	</div>
 	<div class="d-flex justify-content-between">
-		<p class="mb-1 text-truncate text-white">{file.artist}</p>
+		<p class="mb-1 text-truncate text-white">{file.artist ? file.artist : '(Anonym)'}</p>
 		<small class="text-warning">verloren</small>
 	</div>
 </a>
