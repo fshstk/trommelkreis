d200d031f47766e733ffd4e23f79a6f96d072a4b
fshstk <fshstk@fshstk.com>
2023-04-17 14:56:38 +0200

    Add page loader for fetching a single session

---
+++ src/routes/(pages)/archiv/sessions/[slug]/+page.js
@@ -0,0 +1,27 @@
+import { error } from '@sveltejs/kit';
+import dayjs from 'dayjs';
+
+export async function load({ fetch, params }) {
+	async function getSession(slug) {
+		const response = await fetch(`/api/sessions/${slug}`);
+		return await response.json();
+	}
+
+	let session = await getSession(params.slug);
+
+	if (!session) {
+		throw error(404, {
+			message: 'Diese Session existiert nicht.'
+		});
+	}
+
+	let date = dayjs(session.date).format('DD.MM.YYYY');
+	console.log(session);
+
+	return {
+		title: `Challenge vom ${date}`,
+		heading: session.challenge.name,
+		subHeading: date,
+		session: session
+	};
+}
