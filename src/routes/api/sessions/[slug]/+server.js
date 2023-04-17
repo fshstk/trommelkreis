5c7c61d0a6f4d7e4e98555401d1d58f0cc7a4f59
fshstk <fshstk@fshstk.com>
2023-04-17 14:45:06 +0200

    Add new API route for fetching a single session

---
+++ src/routes/api/sessions/[slug]/+server.js
@@ -0,0 +1,54 @@
+import { json } from '@sveltejs/kit';
+import prisma from '$lib/server/prismaclient';
+import dayjs from 'dayjs';
+
+async function formatFile(file) {
+	let artist = await prisma.archive_artist.findUnique({
+		where: {
+			id: file.artist_id
+		}
+	});
+
+	return {
+		name: file.name,
+		url: file.data,
+		artist: artist.name
+	};
+}
+
+async function formatSession(session) {
+	let challenge = await prisma.archive_challenge.findUnique({
+		where: {
+			id: session.challenge_id
+		}
+	});
+
+	let files = await prisma.archive_audiofile.findMany({
+		where: {
+			session_id: session.id
+		}
+	});
+
+	return {
+		date: dayjs(session.date).format('YYYY-MM-DD'),
+		challenge: {
+			name: challenge.name,
+			blurb: challenge.blurb,
+			description: challenge.description
+		},
+		files: await Promise.all(files.map(formatFile))
+	};
+}
+
+export async function GET({ params }) {
+	let session = await prisma.archive_session.findUnique({
+		where: {
+			slug: params.slug
+		}
+	});
+
+	if (!session) return json(null);
+
+	let formatted = await formatSession(session);
+	return json(formatted);
+}
