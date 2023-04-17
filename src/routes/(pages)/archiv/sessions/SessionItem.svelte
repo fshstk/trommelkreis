a5d6d206dd23ba05b553739aad29d318517f4f53
fshstk <fshstk@fshstk.com>
2023-04-17 03:19:30 +0200

    Finish SessionItem/SessionList components

--- src/routes/(pages)/archiv/sessions/SessionItem.svelte
+++ src/routes/(pages)/archiv/sessions/SessionItem.svelte
@@ -5,6 +5,29 @@
 	export let session;
 
 	let date = dayjs(session.date).format('DD.MM.YYYY');
+	let showItem = session.num_entries > 0;
+
+	function numFiles(session) {
+		if (session.num_entries === 0) return 'Keine Einträge';
+		if (session.num_entries === 1) return '1 Eintrag';
+		return `${session.num_entries} Einträge`;
+	}
 </script>
 
-<li>{date} - {session.challenge.name} ({session.num_entries} Einträge)</li>
+{#if showItem}
+	<a href="/archiv/sessions/{session.slug}" class="list-group-item list-group-item-action">
+		<div class="d-flex justify-content-between">
+			<h5 class="mb-1 text-truncate">{session.challenge.name}</h5>
+			<small class="text-muted">{date}</small>
+		</div>
+		<p style="height: 20px;" class="mb-1 text-truncate">{session.challenge.blurb}</p>
+		<div class="d-flex justify-content-between">
+			<small class="text-muted">{numFiles(session)}</small>
+			<!-- 
+    {#if session.copyright_issues}
+			<small><i class="fa-solid fa-lock" /></small>
+		{/if}
+    -->
+		</div>
+	</a>
+{/if}
--- src/routes/(pages)/archiv/sessions/SessionList.svelte
+++ src/routes/(pages)/archiv/sessions/SessionList.svelte
@@ -2,17 +2,38 @@
 	import dayjs from 'dayjs';
 	import 'dayjs/locale/de';
 	import SessionItem from './SessionItem.svelte';
+	import { orderBy } from 'lodash';
 
 	export let yearAndMonth;
 	export let sessions;
 
 	let year = dayjs(yearAndMonth).format('YYYY');
 	let month = dayjs(yearAndMonth).locale('de').format('MMMM');
+
+	let sorted = orderBy(sessions, 'date', ['asc']);
+
+	let leftCol = sorted.slice(0, Math.ceil(sorted.length / 2));
+	let rightCol = sorted.slice(Math.ceil(sorted.length / 2));
 </script>
 
 <h3>{month} <small class="text-muted">{year}</small></h3>
-<ol>
-	{#each sessions as session}
-		<SessionItem {session} />
-	{/each}
-</ol>
+
+<div class="row">
+	<div class="col-md-6">
+		<div class="list-group text-left">
+			{#each leftCol as session}
+				<SessionItem {session} />
+			{/each}
+		</div>
+	</div>
+
+	<div class="col-md-6">
+		<div class="list-group text-left">
+			{#each rightCol as session}
+				<SessionItem {session} />
+			{/each}
+		</div>
+	</div>
+</div>
+
+<hr />
