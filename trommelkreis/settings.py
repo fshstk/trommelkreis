cc8681c398b4fa9595f34ba2c3b381d7430868b8
fshstk <fshstk@fshstk.com>
2024-12-31 02:51:46 +0000

    Switch to S3 storage (only needs settings tweak!)

--- trommelkreis/settings.py
+++ trommelkreis/settings.py
@@ -116,3 +116,11 @@ DATABASES = {"default": dj_database_url.config()}
 SECRET_KEY = os.environ.get("SECRET_KEY", "_")
 
 ALLOWED_HOSTS = "*"
+
+AWS_S3_FILE_OVERWRITE = False
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "_")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "_")
+AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL", "_")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "_")
