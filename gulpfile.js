6dd249b37e846f436553f74838da0c5f81c90a2c
fshstk <fshstk@fshstk.com>
2025-01-03 19:56:59 +0000

    Bump Django from 3.x to ^4.2.8
    This is the first version to support Python 3.12

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ function copy_scripts() {
     const files = [
         "node_modules/jquery/dist/jquery.min.js",
         "node_modules/bootstrap/dist/js/bootstrap.min.js",
+        "node_modules/bootstrap/dist/js/bootstrap.min.js.map",
     ];
     return src(files).pipe(dest("_scripts"));
 }
--- trommelkreis/settings.py
+++ trommelkreis/settings.py
@@ -115,8 +115,6 @@ DATABASES = {"default": dj_database_url.config()}
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "_")
 
-ALLOWED_HOSTS = "*"
-
 AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
