3e825316a16e8ae8d9d57d0ceaa9e8ce808a511c
fshstk <fshstk@fshstk.com>
2025-01-03 23:51:03 +0000

    Fix ALLOWED_HOSTS
    Not having this means we can't serve our site on trommelkreis.club

--- trommelkreis/settings.py
+++ trommelkreis/settings.py
@@ -115,6 +115,8 @@ DATABASES = {"default": dj_database_url.config()}
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "_")
 
+ALLOWED_HOSTS = [".localhost", "127.0.0.1", ".trommelkreis.club"]
+
 AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
