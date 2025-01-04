7bf1eaa1153d15edbd36ca7e638b638c42b86256
fshstk <fshstk@fshstk.com>
2025-01-04 01:33:54 +0000

    Add trommelkreis.club and all subdomains to trusted origins

--- trommelkreis/settings.py
+++ trommelkreis/settings.py
@@ -116,6 +116,7 @@ DATABASES = {"default": dj_database_url.config()}
 SECRET_KEY = os.environ.get("SECRET_KEY", "_")
 
 ALLOWED_HOSTS = [".localhost", "127.0.0.1", ".trommelkreis.club"]
+CSRF_TRUSTED_ORIGINS = ["https://trommelkreis.club", "https://*.trommelkreis.club"]
 
 AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
