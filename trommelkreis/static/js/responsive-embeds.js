6a86944645206ac4c0e5aa63743c48710654e2c0
fshstk <fshstk@fshstk.com>
2025-01-06 14:39:23 +0000

    Remove node completely, serve all CSS/JS via CDN

--- trommelkreis/static/js/responsive-embeds.js
+++ trommelkreis/static/js/responsive-embeds.js
@@ -2,7 +2,7 @@
 
 function replaceYoutubeTagWithEmbed() {
   // select any <youtube> tag inside challenge-description div
-  $("div.challenge-description youtube").each(function () {
+  $("#challenge-description youtube").each(function () {
     let embedCode = $(this).text();
     let embedTag =
       "<div class='embed-container'><iframe src='https://www.youtube.com/embed/" +
